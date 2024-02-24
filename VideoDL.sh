e952352916558ae696dcc89f603e2839f362a8a4
ikrfun <t.nobuto130625@gmail.com>
2024-02-24 17:57:35 +0900

    macでも動作するようになった

---
+++ VideoDL.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Activating Python venv environment..."
+
+# 仮想環境のディレクトリを指定
+VENV_DIR="venv"
+
+# 仮想環境をアクティベート
+source $VENV_DIR/bin/activate
+
+# Pythonスクリプトを実行
+python3 videoDL2.py
+
+echo "Application has been executed successfully."
+read -p "Press [Enter] key to close..."
